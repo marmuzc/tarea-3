58d93da83344feb48b3d3e17cff61bccb266ec27
Mariel M <mariemunoz2022@udec.cl>
2024-10-27 22:47:09 -0300

    Modificado el deposito de productos para que solo vaya recibiendo de un producto a la vez, agregado metodo getproducto

--- src/main/java/LOGICA/Depositos/DepositoP.java
+++ src/main/java/LOGICA/Depositos/DepositoP.java
@@ -6,14 +6,23 @@ import LOGICA.Productos.*;
  * Hereda de la clase genérica {@code Deposito} para manejar objetos del tipo {@code Productos}.
  */
 public class DepositoP extends Deposito<Productos> {
+    private int capacidadMaxima;
 
     /**
-     * Constructor de la clase {@code DepositoP}.
-     * Inicializa el depósito de productos llamando al constructor de la clase padre.
+     * Constructor de la clase {@code DepositoP} con capacidad limitada.
+     *
+     * @param capacidadMaxima La cantidad máxima de productos que puede almacenar el depósito.(en la tarea se pide que sea 1)
      */
+    public DepositoP(int capacidadMaxima) {
+        super(); // Llama al constructor del padre
+        this.capacidadMaxima = capacidadMaxima;
+    }
+    
     public DepositoP() {
         super();
+        this.capacidadMaxima = 1;
     }
+
 
     /**
      * Agrega un producto al depósito.
@@ -21,7 +30,11 @@ public class DepositoP extends Deposito<Productos> {
      * @param producto El producto que se desea agregar al depósito.
      */
     public void addProducto(Productos producto) {
-        super.addDeposito(producto);  // Utiliza el método de la clase padre para agregar un producto
+        if (getDeposito().size() < capacidadMaxima) {
+            super.addDeposito(producto);  // Utiliza el método de la clase padre para agregar un producto
+        } else {
+            System.out.println("Depósito lleno, no se puede agregar más productos.");
+        }
     }
 
     /**
@@ -31,6 +44,6 @@ public class DepositoP extends Deposito<Productos> {
      */
     @Override
     public Productos getProducto() {
-        return super.getProducto();  // Utiliza el método de la clase padre para obtener un producto
+        return super.getProducto();
     }
 }
